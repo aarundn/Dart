90b917e919ef3efba7dff5f9cf28ba05b32dbe50
aarundn <debchouneharoun@gmail.com>
2025-05-05 16:46:26 +0200

    add libs

--- statments.dart
+++ statments.dart
@@ -1,7 +1,15 @@
+import 'dart:math';
+
 void main(){
   var age = 20;
   var flybyObjects = ['Earth', 'Mars', 'Venus'];
   var year = 2000;
+
+  var random = Random();
+  print(random.nextInt(10));
+
+  flybyObjects.where((name) => name.contains('Earth')).forEach(print);
+
   if (age >= 18) {
     print('You are an adult.');
   } else {
