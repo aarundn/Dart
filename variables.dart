f014f1f94276885cfe617fde27b86c25fe3df5e4
aarundn <debchouneharoun@gmail.com>
2025-05-05 16:19:06 +0200

    variables in dart

---
+++ variables.dart
@@ -0,0 +1,16 @@
+void main(){
+
+  var a = 10;
+  var b = 20.5;
+  var c = 'Hello';
+  var d = true;
+  var prices = [10.5, 20.5, 30.5];
+  var names = ['haroun', 'debchoune', 'dart'];
+
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+  print(prices);
+  print(names);
+}
